d33f65c830e80489d406737d2d45c0ba1b1d4e63
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:18:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,12 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name}
       resp.write item.price
+
+    elsif req.path.match(/items/)
+        item_name = req.path.split("/items/").last
+        if @@items.exclude?(item_name)
+        resp.write("Item not found")
+
     elsif req.path.match(/testing/)
       resp.write("Route not found")
       resp.status = 404
