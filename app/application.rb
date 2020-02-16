0b235bfdef8286d2dfa1a7cbaa62939a406c359d
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:39:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,7 @@ class Application
     elsif req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
+      binding.pry
       resp.write item.price
     else
         resp.write("Item not found")
