ae39eb6b5ec5e4e3be6deb040dfc383fe5246dfe
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:14:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name}
+      resp.write item.price
+    else
+      resp.write("Route not found")
+      resp.status = 404
+    end
 
   end
 
