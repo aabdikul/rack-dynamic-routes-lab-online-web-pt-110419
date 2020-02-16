4bf363aaad47157f70ac1308f56c525c0da0e871
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:50:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,6 @@ class Application
     elsif req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-      binding.pry
       resp.write item.price
 
     elsif req.path.match(/items/)
