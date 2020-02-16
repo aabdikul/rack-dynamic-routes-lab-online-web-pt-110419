a715aad0a98e37200b564bca4eaca02f7406ec14
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:23:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write item.price
     elsif req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      @@items.include?(item_name ) == false
+      @@items.include?(item_name) == false
         resp.write("Item not found")
         resp.status = 400
     end
