278e715235ca63d72f76e84f4f06d3805d6c3386
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:13:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,13 @@ class Application
 @@items = []
 
   def call(env)
-    if
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|item| item.name}
+
   end
 
 end
