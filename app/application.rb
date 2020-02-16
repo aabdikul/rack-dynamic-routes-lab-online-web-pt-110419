c6d9595d2bcca3d2e052b782a4d334022878e95b
Aida Abdikulova <abdikulova@gmail.com>
2020-02-16 07:33:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,7 @@ class Application
         if item.include?(item_name)
         resp.write item.price
       end
+    end
       else
         resp.write("Item not found")
         resp.status = 400
